1803eb9d6bfebf0e03f79a4d6e687ee92c310b5c
Nathan Collins <crossfields.tech@gmail.com>
2021-07-01 08:34:00 +1000

    full screen now working
    attempted resources, still some work to do on resources

--- src/game.js
+++ src/game.js
@@ -1,24 +1,19 @@
 import * as me from "https://melonjs-builds.s3.amazonaws.com/artifacts/master/2839/build/melonjs-module.js";
 
 const game = {
+  resources: [{ name: "bg", type: "image", src: "assets/images/bg.jpeg" }],
+
   onload: () => {
-    if (!me.video.init(1024, 768, { scaleMethod: "flex" })) {
+    if (
+      !me.video.init(1280, 1280, {
+        wrapper: "screen",
+        scale: "auto",
+        scaleMethod: "flex",
+      })
+    ) {
       alert("Your browser does not support HTML5 canvas.");
       return;
     }
-
-    me.game.world.addChild(new me.ColorLayer("background", "#000000"));
-
-    me.game.world.addChild(
-      new me.Text(609, 201, {
-        font: "Arial",
-        size: 160,
-        fillStyle: "#FFFFFF",
-        textBaseline: "middle",
-        textAlign: "center",
-        text: "New Title!",
-      })
-    );
 
     me.audio.init("mp3,ogg");
     debugger;
@@ -38,6 +33,19 @@ const game = {
       }
       me.device.exitFullscreen();
     });
+
+    me.game.world.addChild(new me.ColorLayer("background", "#000000"));
+
+    me.game.world.addChild(
+      new me.Text(700, 100, {
+        font: "Arial",
+        size: 160,
+        fillStyle: "#FFFFFF",
+        textBaseline: "middle",
+        textAlign: "center",
+        text: "New Title!",
+      })
+    );
   },
 };
 
